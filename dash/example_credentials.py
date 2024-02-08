792780041d3954a8602e187d541159b006813e3c
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2024-02-08 15:26:04 +1300

    Prepare for public release

--- dash/example_credentials.py
+++ dash/example_credentials.py
@@ -1,5 +1,31 @@
-# Calendar Links
-calendar_links = {
-    "Name 1": "url1",
-    "Name 2": "url2"
+# Calendar links
+
+# Timetable
+timetable_links = {
+    ### NOTE THE ORDER OF NAMES IN DICT MUST STAY THE SAME! ###
+    # Must USE _ UNDERSCORES, NO SPACES
+    "Name1": "",
+    "Name_Two": "",
+    "Name_Three": "",
+    "Name_Four": "",
 }
+
+initials = {
+    "AB": "Name1",
+    "CD": "Name_Two",
+    "EFG": "Name_Three",
+    "H": "Name_Four"
+}
+
+# This calendar will be split by events starting wtih initials.
+combined_cal = ""
+
+personal_cals = {}
+
+quote_url = "https://zenquotes.io/api/today"
+
+
+# Set a testing date if you want the calendar to render a different date.
+# NOTE this doesn't affect date display/percentage etc.
+testing_date = False
+# testing_date = (2024,2,29)
