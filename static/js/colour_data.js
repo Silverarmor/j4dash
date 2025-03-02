873de1ca8b880b0361498713357aeac82d5e3f70
Jayden <23619946+Silverarmor@users.noreply.github.com>
2025-03-02 21:46:15 +1300

    Update colours for 2025 sem1 courses

--- static/js/colour_data.js
+++ static/js/colour_data.js
@@ -13,10 +13,16 @@ let JK_colours = {
     // "STRCTENG 200": "rgb(243, 182, 31)", //yellow
     // "ENGSCI 211": "rgb(100, 100, 120)", // bluey grey
 
-    "CIVIL 200": "rgb(255, 110, 50)", //ff6e32 orange
-    "ENVENG 200": "rgb(151, 212, 80)", //c2fa78 light green
-    "STRCTENG 201": "rgb(243, 182, 31)", //yellow
-    "ENGGEN 204": "rgb(100, 100, 120)", // bluey grey
+    // "CIVIL 200": "rgb(255, 110, 50)", //ff6e32 orange
+    // "ENVENG 200": "rgb(151, 212, 80)", //c2fa78 light green
+    // "STRCTENG 201": "rgb(243, 182, 31)", //yellow
+    // "ENGGEN 204": "rgb(100, 100, 120)", // bluey grey
+
+    "CIVIL 300": "rgb(255, 110, 50)", //ff6e32 orange
+    "ENVENG 300": "rgb(151, 212, 80)", //c2fa78 light green
+    "STRCTENG 304": "rgb(243, 182, 31)", //yellow
+    "ENGGEN 303": "rgb(100, 100, 120)", // bluey grey
+
 
 
     "default": "rgb(248, 58, 34)", // dark red
@@ -27,11 +33,13 @@ let JP_colours = {
 
     "POM: Lecture": "#B8E28D", // Light green
     "PCS1: Lecture": "#35827C", //lighter grey aka teal
+    "NS: Lecture": "#35827C", //lighter grey aka teal
 
     "MSS: Lecture": "#A97591", // pinky
     "DS: Lecture": "#A97591", // pinky
     "CVS: Lecture": "#A97591", // pinky
-
+    "321": "#A97591", // pinky
+    
     "POM Tbl": "#2E933C", // forest/mid green
     "PCS SGA": "#204E4A", // dark slate grey
 
@@ -46,11 +54,13 @@ let JAC_colours = {
     // https://coolors.co/065143-3badba-41b49b-674ea7
 
     "POM: Lecture": "#4BAA74",  // jade
+    "321": "#4BAA74",  // jade
 
     "PCS1: Lecture": "#83CFD8", // light blue
     "MSS: Lecture": "#87D4C3", // light blue/green
     "DS: Lecture": "#87D4C3", // light blue/green
     "CVS: Lecture": "#87D4C3", // light blue/green
+    "NS": "#87D4C3", // light blue/green
 
     "POM Tbl": "#065143", // green
     "PCS SGA": "#3BADBA",  //bluey moonstone
@@ -64,13 +74,22 @@ let JAC_colours = {
 
 let JT_colours = {
     // https://coolors.co/e23c4a-00e0ac-7494ea-cfa5a0-044b7f
-    "COMPSCI 110 TUT": "#C31D2B", // Darker red
-    "COMPSCI 230 LAB": "#2A5ADF", // Darker blue
-    "COMPSCI 110": "#E23C4A", //red
-    "COMPSCI 230": "#7494EA", //blue
-    "LAW 201": "#CFA5A0", //LAW 201A matches this Rosy brown
-    "LAW 211": "#00E0AC", //LAW 211A matches this. Aquamarine
-    "LAW 298": "#044B74", //LAW 298A matches this. Indigo
+    // "COMPSCI 110 TUT": "#C31D2B", // Darker red
+    // "COMPSCI 230 LAB": "#2A5ADF", // Darker blue
+    // "COMPSCI 110": "#E23C4A", //red
+    // "COMPSCI 230": "#7494EA", //blue
+    // "LAW 201": "#CFA5A0", //LAW 201A matches this Rosy brown
+    // "LAW 211": "#00E0AC", //LAW 211A matches this. Aquamarine
+    // "LAW 298": "#044B74", //LAW 298A matches this. Indigo
+
+
+    "COMPSCI 215 LAB": "#C31D2B", // Darker red
+    "PHYSICS 140 LAB": "#2A5ADF", // Darker blue
+    "COMPSCI 215": "#E23C4A", //red
+    "PHYSICS 140": "#7494EA", //blue
+    "LAW 231": "#CFA5A0", //LAW 231A matches this Rosy brown
+    "LAW 241": "#00E0AC", //LAW 241A matches this. Aquamarine
+    "LAWGENRL 433": "#044B74", // matches this. Indigo
 
     "default": "#6d9eeb",
 }
